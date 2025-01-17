6f5921a23ae1a824963eb7b72ac0641b40407315
Misho1019 <mikailtorres99@gmail.com>
2025-01-17 07:01:48 +0200

    Add dynamic movie rendering of home page

---
+++ src/movies.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: 'Jungle Cruise',
+        genre: 'Adventure',
+        description: 'Dreaming about saving countless lives and having another adventure, the feisty English feminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change the world. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant, wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine Amazon River in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper into the heart of an impenetrable green maze, searching for something that cannot be found, a centuries-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to their ambitious plans.',
+        imageUrl: './static/img/jungle-cruise.jpeg',
+    },
+    {
+        title: 'Ask Mantik Intikam',
+        genre: 'Comedy',
+        description: 'A couple gets divorced after several years of marriage. After that, the man suddenly gets rich, and life changes again when his ex-wife starts working with him.',
+        imageUrl: 'https://tse2.mm.bing.net/th/id/OIP.6ASwlTmrCfIGbM_zytjvBgHaGv?w=198&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    }
+]
