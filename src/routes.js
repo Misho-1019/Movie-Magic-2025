386cb96f7624ed6872d6acc84a723e696c7216a6
Misho1019 <mikailtorres99@gmail.com>
2025-01-18 04:41:12 +0200

    Add controller and create page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import homeController from "./controllers/home-controller.js";
+import movieController from "./controllers/movieController.js";
 
 const routes = Router();
 
 routes.use(homeController)
+routes.use(movieController)
 
 routes.all('*', (req, res) => {
     res.render('404')
