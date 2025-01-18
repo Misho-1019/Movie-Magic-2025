dce505d7588e01f812f8ca5dcb5e56147e3d773d
Misho1019 <mikailtorres99@gmail.com>
2025-01-18 04:49:33 +0200

    Change movies controller base url

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import movieController from "./controllers/movieController.js";
 const routes = Router();
 
 routes.use(homeController)
-routes.use(movieController)
+routes.use('/movies', movieController)
 
 routes.all('*', (req, res) => {
     res.render('404')
