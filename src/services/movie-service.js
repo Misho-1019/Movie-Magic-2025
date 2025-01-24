d2a3540b340c5a075eeac187feeefc011e426a24
Misho1019 <mikailtorres99@gmail.com>
2025-01-24 06:24:20 +0200

    Refactor home page to use database

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,26 +1,26 @@
-import movies from "../movies.js";
+import Movie from "../models/Movie.js";
 import { v4 as uuid } from "uuid";
 
 export default {
     getAll(filter = {}) {
-        let results = movies;
+        let results = Movie.find({});
         
-        if (filter.search) {
-            results = results.filter(movie => movie.title.toLocaleLowerCase().includes(filter.search.toLocaleLowerCase()))
-        }
+        // if (filter.search) {
+        //     results = results.filter(movie => movie.title.toLocaleLowerCase().includes(filter.search.toLocaleLowerCase()))
+        // }
 
-        if (filter.genre) {
-            results = results.filter(movie => movie.genre.toLocaleLowerCase() === filter.genre.toLocaleLowerCase())
-        }
+        // if (filter.genre) {
+        //     results = results.filter(movie => movie.genre.toLocaleLowerCase() === filter.genre.toLocaleLowerCase())
+        // }
 
-        if (filter.year) {
-            results = results.filter(movie => movie.year == filter.year)
-        }
+        // if (filter.year) {
+        //     results = results.filter(movie => movie.year == filter.year)
+        // }
 
         return results;
     },
-    findOne(movieId) {
-        const result = movies.find(movie => movie.id === movieId)
+    getOne(movieId) {
+        const result = Movie.findById(movieId)
 
         return result;
     },
