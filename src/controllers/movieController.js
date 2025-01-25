855558f8c166e7bb419defe42bf840308caf0429
Misho1019 <mikailtorres99@gmail.com>
2025-01-25 06:05:42 +0200

    Attach cast to movie first method

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -43,4 +43,15 @@ movieController.get('/:movieId/attach-cast', async (req, res) => {
     res.render('movie/attach-cast', { movie, casts })
 })
 
+movieController.post('/:movieId/attach-cast', async (req, res) => {
+    console.log(req.body);
+    
+    const castId = req.body.cast;
+    const movieId = req.params.movieId;
+    
+    await movieService.attachCast(movieId, castId)
+
+    res.redirect(`/movies/${movieId}/details`)
+})
+
 export default movieController;
--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -32,5 +32,13 @@ export default {
         })
 
         return result;
+    },
+    async attachCast(movieId, castId) {
+        const movie = await Movie.findById(movieId)
+        movie.casts.push(castId)
+
+        await movie.save()
+
+        return movie;
     }
 }
