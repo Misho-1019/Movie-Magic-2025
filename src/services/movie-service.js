d295b1cc721ca94c724145a635a303dfe8b6f17b
Misho1019 <mikailtorres99@gmail.com>
2025-02-03 18:37:10 +0200

    Update movie from edit

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -42,5 +42,8 @@ export default {
     },
     delete(movieId) {
         return Movie.findByIdAndDelete(movieId)
+    },
+    update(movieId, movieData) {
+        return Movie.findByIdAndUpdate(movieId, movieData);
     }
 }
