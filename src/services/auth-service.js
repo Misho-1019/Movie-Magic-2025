7ad1743788f75f33ff98d4cf35488a2f264c9d83
Misho1019 <mikailtorres99@gmail.com>
2025-02-07 06:30:19 +0200

    Check repeat password

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -5,7 +5,17 @@ import jwt from "jsonwebtoken";
 const SECRET = process.env.JWT_SECRET || 'BASICSECRET';
 
 export default {
-    register(userData) {
+    async register(userData) {
+        
+        // if (userData.password !== userData.rePassword) {
+        //     throw new Error('Password mismatch!')
+        // }
+        
+        const userCount = await User.countDocuments({ email: userData.email})
+        if (userCount > 0) {
+            throw new Error('Email already exists')
+        }
+
         return User.create(userData)
     },
     async login(email, password) {
