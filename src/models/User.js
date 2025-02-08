524e0c788eb5cde6e31b80be6a76a3362020da58
Misho1019 <mikailtorres99@gmail.com>
2025-02-08 03:56:04 +0200

    Modify Movie and User models validations

--- src/models/User.js
+++ src/models/User.js
@@ -4,14 +4,17 @@ import bcrypt from "bcrypt";
 const userSchema = new Schema({
     email: {
         type: String,
+        required: true,
         unique: true,
+        lowercase: true,
         match: /\@[a-zA-Z]+.[a-zA-Z]+$/,
         minLength: 10,
     },
     password: {
         type: String,
+        minLength: [6, 'Password should be at least 6 characters'],
         match: /^\w+$/,
-        minLength: 6,
+        trim: true,
     },
 })
 
